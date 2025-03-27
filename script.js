fe77f383238330a051d8d68691ebf9d5861c3ca7
just1jray <24859765+just1jray@users.noreply.github.com>
2025-03-26 23:45:51 -0400

    Add color modes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function createGrid(gridSize) {
+function createGrid(gridSize, color) {
     const grid = document.getElementById('grid-container');
 
     for (let i = 0; i < gridSize * gridSize; i++) {
@@ -7,13 +7,30 @@ function createGrid(gridSize) {
         square.style.height = `calc(100% / ${gridSize})`;
         square.style.width = `calc(100% / ${gridSize})`;
         square.addEventListener('mouseenter', () => {
-            square.style.backgroundColor = "grey";
+            if (color == "random") {
+                square.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+            } else if (color == "shade") {
+                square.style.backgroundColor = "black";
+                let currentOpacity = Number(square.style.opacity) || 0;
+                square.style.opacity = Math.min(currentOpacity + 0.2, 1);
+            } else {
+                square.style.backgroundColor = color;
+            }
         });
         grid.appendChild(square);
     }
 }
 
+function resetGrid() {
+    const grid = document.getElementById('grid-container'); 
+    while (grid.firstChild) {
+        grid.removeChild(grid.lastChild);
+    }
+}
+
 let gridSize = 16;
+const colors = ["grey", "random", "shade"]
+let color = colors[0]
 
 const main = document.getElementById('main-container');
 
@@ -28,22 +45,22 @@ button.addEventListener('click', () => {
     } else {
         gridSize = size
     }
-    const grid = document.getElementById('grid-container'); 
-    while (grid.firstChild) {
-        grid.removeChild(grid.lastChild);
-    }
-    createGrid(gridSize);
+    resetGrid()
+    createGrid(gridSize, color);
 });
 main.insertBefore(button, main.firstChild);
 
-createGrid(gridSize)
+createGrid(gridSize, color)
 
 const circles = document.getElementsByClassName('circle')
-for (let i = 0; i < circles.length; i++) {
-    circles[i].addEventListener('click', () => {
-        const squares = document.getElementsByClassName('grid-square');
-        for (let j = 0; j < squares.length; j++) {
-            squares[j].style.backgroundColor = "lightgrey";
-        }
-    });
-}
+
+circles[0].addEventListener('click', () => {
+    color = colors[(colors.indexOf(color) + 1) % colors.length];
+    resetGrid()
+    createGrid(gridSize, color);
+});
+
+circles[1].addEventListener('click', () => {
+    resetGrid()
+    createGrid(gridSize, color);
+});
